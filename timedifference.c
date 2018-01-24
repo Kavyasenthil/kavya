854adfe35c1092626c557c4d653ba93a3a991d97
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-01-24 16:51:17 +0530

    Update timedifference.c

--- timedifference.c
+++ timedifference.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
 int main()
 {
-int h1,m1,h2,m2,h,m;
+int h1,m1,h2,m2,hour,min,h;
 printf("enter the 2 time values in hours and min format");
 scanf("%d %d %d %d",&h1,&m1,&h2,&m2);
-h=h1-h2;
-m=m1-m2;
-printf("%d:%d is the time value",h,m);
+hour=h1-h2;
+if(m1<m2)
+{
+min=m1-m2;
+min+=60;
+++hour;
+}else
+min=m1-m2;
+printf("%d:%d is the time value",hour,min);
 return(0);
 }
