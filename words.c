6b85728d2290db448fe45374b7f08e7488066562
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-01-31 16:12:45 +0530

    Create words.c

---
+++ words.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int i,s,words,count=0;
+char a[200];
+printf("\n enter the sentence:");
+scanf("%[^\n]s",&a);
+s=strlen(a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ')
+{
+count++;
+words=count+1;
+}
+}
+printf("\n no. of words: %d",words);
+return 0;
+}
