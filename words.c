94b32a582348b257cc440bb5234ed0e1508a86bb
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-01-31 16:14:57 +0530

    Update words.c

--- words.c
+++ words.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 int main()
 {
-int i,s,words,count=0;
+int i,words,count=0;
 char a[200];
 printf("\n enter the sentence:");
 scanf("%[^\n]s",&a);
-s=strlen(a);
 for(i=0;a[i]!='\0';i++)
 {
 if(a[i]==' ')
