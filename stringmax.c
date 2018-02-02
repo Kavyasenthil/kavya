7bae3b9e100c53c125e7a9cc010e6565f3a0b38d
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-02-02 16:32:55 +0530

    Create stringmax.c

---
+++ stringmax.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+
+int main(void) {
+	char a[20],b[20];
+	int al,bl;
+	printf("\n enter the 2 strings");
+	scanf("%s %s",&a,&b);
+	al=strlen(a);
+	bl=strlen(b);
+	if(al>bl)
+	{
+		printf("\n %s",a);
+	}
+	else
+	{
+		printf("\n %s",b);
+	}
+	return 0;
+}
