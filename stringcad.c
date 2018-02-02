afa2162ae222cd6ccdcdc8e154601da4ce8f53f2
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-02-02 16:43:57 +0530

    Create stringcad.c

---
+++ stringcad.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<string.h>
+
+int main(void) {
+	char a[20],b[20];
+	printf("\n enter the 2 strings");
+	scanf("%s %s",&a,&b);
+	printf("\n concadenated string is %s%s",a,b);
+	return 0;
+}
