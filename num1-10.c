7acc81ae6e0205441de1c6cf6118737998c50dc6
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-02-05 15:36:21 +0530

    Create num1-10.c

---
+++ num1-10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+int a;
+printf("\n enter the number");
+scanf("%d",&a);
+if(a>0&&a<=10)
+{
+	printf("\n yes");
+}else
+{
+	printf("\n No");
+}
+	return 0;
+}
