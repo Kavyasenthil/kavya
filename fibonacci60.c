53f247242d062a6b1f0bda3c3a5b4ad189fe3dd1
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-02-07 16:36:38 +0530

    Create fibonacci60.c

---
+++ fibonacci60.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+int a,i,first=0,second=1,sum;
+printf("\n enter the limit");
+scanf("%d",&a);
+printf("\n the fibonacci series is");
+for(i=0;i<a;i++)
+{
+	sum=first+second;
+	first=second;
+	second=sum;
+	printf("\t %d",f);
+}	return 0;
+}
