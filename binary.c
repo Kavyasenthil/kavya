e3a87daf957481a0319e5d8cc0cd6c020e787073
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-02-09 15:51:30 +0530

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,a,count=0,flag=0;
+	printf("\n enter the value");
+	scanf("%d",&n);
+	while(n>0)
+	{
+		a=n%10;
+		count++;
+		if(a==1||a==0)
+		flag++;
+		n=n/10;
+	}
+	if(count==flag)
+	printf("\n yes");
+	else
+	printf("\n no");
+	return 0;
+}
