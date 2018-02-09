996a6ee8e3992dd9f6b13cc8b0103fec7c69c400
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-02-09 16:51:04 +0530

    Create suboddoreven.c

---
+++ suboddoreven.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a,b,c;
+    printf("\n enter the 2 nums");
+    scanf("%d %d",&a,&b);
+    c=a-b;
+    if(c%2==0)
+    printf("\n even");
+    else
+    printf("\n odd");
+	return 0;
+}
