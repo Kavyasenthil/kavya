0177a799e7bca2eef57a266dca55fed19ec88914
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-02-19 16:42:05 +0530

    Create floatround.c

---
+++ floatround.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+int n;
+float a;
+printf("\n enter the number");
+scanf("%f",&a);
+n=a;
+if(n>a)
+printf("\n %d",n);
+else
+printf("\n %d",n+1);
+	return 0;
+}
