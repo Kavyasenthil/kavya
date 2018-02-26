2d859dedea2e9f3b4354c6fbb73b942979ef0aca
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-02-26 16:09:43 +0530

    Create perfectsquare.c

---
+++ perfectsquare.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a,b;
+	printf("\n enter the 2 numbers");
+	scanf("%d %d",&a,&b);
+		if(a==b)
+		printf("\n %d is a perfect square",a*b);
+		else 
+		printf("\n %d is not a perfect square",a*b);
+	
+
+	return 0;
+}
