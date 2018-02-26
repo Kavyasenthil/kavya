1c20f27b7bb9e7300f42b473911a973e1aaffc79
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-02-26 16:33:31 +0530

    Create ninjas.c

---
+++ ninjas.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,i;
+	printf("\n enter the num of ninjas in kabali and opposite group");
+	for(i=0;i<32;i++)
+	{
+	scanf("%d %d",&a,&b);
+		
+	}
+	
+	printf("\n the difference is");
+	if(b!=0)
+	{
+	printf("%d\n",b-a);
+	}
+		
+	
+	return 0;
+}
