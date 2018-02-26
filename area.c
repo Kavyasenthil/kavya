d3fbf73930ef80b1f1c2cdf27920ba788c51838c
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-02-26 16:39:34 +0530

    Create area.c

---
+++ area.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+	float l,b,area;
+	printf("\n enter the length and breadth");
+	scanf("%5f %5f",&l,&b);
+	printf(" the area of farm is %.5f",l*b);
+	return 0;
+}
