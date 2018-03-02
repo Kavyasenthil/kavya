3c118fafc56e286e24040a0c3d71a07e322b0b2e
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-03-02 15:43:58 +0530

    Create order.c

---
+++ order.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[20];
+    int i,j,n,l;
+    printf("\n enter the character");
+    scanf("%s",&a);
+    l=strlen(a);
+    for(i=0;i<l;i++)
+    {
+        for(j=i+1;j<l;j++)
+        {   if(a[i]>a[j])
+            {
+            n=a[i];
+            a[i]=a[j];
+            a[j]=n;
+            }
+        }
+    }
+    printf("\n the order is");
+    for(i=0;i<l;i++)
+    {
+        printf("\t%c",a[i]);
+    }
+}
