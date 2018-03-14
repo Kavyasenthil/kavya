2dc065c1a40513261178875ce058ef8bcd8727ce
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-03-14 16:53:45 +0530

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,c,ans;
+	printf("\n enter the 3 numbers");
+	scanf("%d%d%d",&a,&b,&c);
+	ans=(a*b)/c;
+	printf("\nthe output is %d",ans);
+	return 0;
+}
