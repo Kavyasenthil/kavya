1d6b221f9c7b0a154f7b63d9c3298e20fe7478ba
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-03-16 16:31:41 +0530

    Update lcm.c

--- lcm.c
+++ lcm.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
  int main(void) 
 {
-	int a,b,i;
+	int a,b,i,t,lcm;
 	printf("\n enter the two numbers");
 	scanf("%d %d",&a,&b);
+	printf("\n LCM is");
 	for(i=2;i<=a&&i<=b;i++)
 	{
 		if(a%i==0&&b%i==0)
 		{
-			printf("\n LCM is %d",i);
-			break;
+			t=i;
 		}
-		else
-		{
-			printf("\n enter valid input");
-			break;
-		}
+		lcm=(a*b)/t;
+		printf("%d",lcm);
+		
 	}
 	return 0;
 }
