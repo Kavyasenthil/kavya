ede6833a96d95872d0b2a0f86692db30a43c08f0
Kavyasenthil <35130176+Kavyasenthil@users.noreply.github.com>
2018-03-21 15:41:53 +0530

    Create k.c

---
+++ k.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(void) {
+int a,b,i,c=0,s[10];
+printf("\n enter the 2 numbers");
+scanf("%d %d",&a,&b);
+int 
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+if(a[i]==m)
+{
+printf("yes");
+flag=1;
+break;
+}
+}
+if(flag==0)
+{
+printf("no");
+}
+return 0;
+}
